bd799fbc384b94cb2cd4744e6acde8127ab369bb
oskogstad <jorgen.skogstad@gmail.com>
2017-01-28 23:13:01 +0100

    made loop test, does not sound good. Also strange stuff with channel 0/-1, maybe

--- src/audio/soundsystem.d
+++ src/audio/soundsystem.d
@@ -4,10 +4,13 @@ Mix_Chunk* music01;
 
 void setup()
 {
+	Mix_ReserveChannels(3);
 	// load all chunks
 	music01 = Mix_LoadWAV("sfx/music/menu_pad01_arp.wav");
 	assert(music01);
-	Mix_PlayChannel(-1, music01, -1);
+	Mix_PlayChannel(1, music01, -1); 
+	/// when playing on reserved chan 0, or -1,
+	// the loop stops when entering game 
 }
 
 void updateAndPlay()
